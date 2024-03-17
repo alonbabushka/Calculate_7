18a66f22e99922deefc48fe2561d1531344e62d7
alonbabushka <163703002+alonbabushka@users.noreply.github.com>
2024-03-17 04:29:19 +0100

    Create calculate.py

---
+++ calculate.py
@@ -0,0 +1,12 @@
+import random
+ 
+# Uncomment the line below to use a seed if needed
+#random.seed(config["seed"])
+ 
+def main(config):
+# StartDate - is a column name
+    if str(StartDate) == '':
+        return DBNull.Value
+ 
+# Add number of days to a StartDate
+return StartDate.AddDays(random.randint(1, 1000));
